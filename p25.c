2fbaddf2cf5f78e68c4101c494cc30465a4279b4
hucuyuu <huangcaoyang@hotmail.com>
2020-09-20 19:05:07 +0800

    algorithm imporoved

--- p25.c
+++ p25.c
@@ -127,20 +127,20 @@ int ListOrder_L(LinkList head)
 {
 	LinkList cmp, cmpp, mov;
 
-	ListPint_L2(head);
+	// ListPint_L2(head);
 
 	cmp = head->next->next;
 	cmpp = head->next;
-	if (cmpp->data > cmp->data) {
-		cmpp->next = cmp->next;
-		cmp->next = head->next;
-		head->next = cmp;
-		cmp = cmpp->next;
-	}
-
-
-	cmp = head->next->next->next;
-	cmpp = head->next->next;
+	// if (cmpp->data > cmp->data) {
+	// 	cmpp->next = cmp->next;
+	// 	cmp->next = head->next;
+	// 	head->next = cmp;
+	// 	cmp = cmpp->next;
+	// }
+	//
+	//
+	// cmp = head->next->next->next;
+	// cmpp = head->next->next;
 	do
 	{
 		// ListPint_L2(head);
@@ -192,7 +192,7 @@ int main()
 	head = (LinkList)malloc(sizeof(LNode));
 	head->next = NULL;
 	CreateList_L(head);
-	ListPint_L2(head);
+	// ListPint_L2(head);
 	do
 	{
 		printf("i,I...Insert\n");
